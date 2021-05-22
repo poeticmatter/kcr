c957dfca90453ec7fbd65b965b844c7e3457dc0d
Aurore <d00m.poet@gmail.com>
2021-05-22 23:06:36 +0300

    Firebase app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database";
+
+import {cards} from "./cards";
+
+class App extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+
+
+  }
+
+  componentDidMount() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyD-N9w8_L7EWZ9ixT7mvT5gwbcKLak5JOA",
+      authDomain: "keyforge-crowdsource-ranking.firebaseapp.com",
+      databaseURL: "https://keyforge-crowdsource-ranking-default-rtdb.europe-west1.firebasedatabase.app",
+      projectId: "keyforge-crowdsource-ranking",
+      storageBucket: "keyforge-crowdsource-ranking.appspot.com",
+      messagingSenderId: "933315858425",
+      appId: "1:933315858425:web:72008c40f28a61ba37b728",
+      measurementId: "G-91F6G5L27G"
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }else {
+      firebase.app();
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        hello?
+      </div>
+    );
+  }
 }
 
 export default App;
